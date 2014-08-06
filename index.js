fc9e81475cd0a4cb0eab8260a45e85623c7f9e29
Artur Zayats <zag2art@gmail.com>
2014-08-06 16:22:36 +0400

    Fix to auto-include component's *.styl files...
    After the fix we will able to auto-include compent's style files like that:
    ```js
    app.loadStyles(path.join(__dirname, '../styles'), {
      use: function(s){
        Object.keys(app.views.nameMap).forEach(function(viewName){
          var view = app.views.nameMap[viewName];
    
          if (view.componentFactory){
            var style = view.componentFactory.constructor.prototype.style;
            if (style) s.include(style);
          }
        });
      }
    });
    ```

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ function stylusCompiler(file, filename, options) {
   options || (options = {});
   options._imports || (options._imports = []);
   var out = {};
-  stylus(file, options)
+  var s = stylus(file, options)
     .use(nib())
     .set('filename', filename)
     .set('compress', options.compress)
-    .set('include css', true)
-    .render(function(err, value) {
-      if (err) throw err;
-      out.css = value;
-    });
+    .set('include css', true);
+    
+  if (options.use){
+    s.use(options.use);
+  }
+  
+  s.render(function(err, value) {
+    if (err) throw err;
+    out.css = value;
+  });
   out.files = options._imports.map(function(item) {
     return item.path;
   });
