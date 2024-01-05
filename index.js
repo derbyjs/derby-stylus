b522899b2dd8936d4506d58d2906b70b087d3a80
Craig Beck <luckybonza@gmail.com>
2024-01-05 11:14:47 -0800

    Add options to provide include paths and plugins like nib

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 var stylus  = require('stylus');
+var path    = require('path');
+var fs      = require('fs');
 
-module.exports = function(app) {
+module.exports = function(app, options = {}) {
+  var { includes = [], plugins = [], ...opts } = options;
+  
+  var stylusRequirePath = require.resolve('stylus');
+  var nodeModulesRoot = stylusRequirePath.substring(0, stylusRequirePath.indexOf('/stylus'));
+
+  var includePaths = includes.reduce((acc, moduleId) => {
+    var stylusModulePath = path.join(nodeModulesRoot, moduleId);
+    return fs.existsSync(stylusModulePath)
+      ? [...acc, stylusModulePath]
+      : acc;
+  }, []);
+
+  function stylusCompiler(file, filename, options) {
+    var options = { _imports: [], ...options, ...opts };
+    var out = {};
+    var compiler = stylus(file, options)
+      .set('paths', includePaths)
+      .set('filename', filename)
+      .set('compress', options.compress)
+      .set('include css', true)
+
+    plugins.forEach(plugin => compiler.use(plugin()));
+      
+    compiler.render(function(err, value) {
+        if (err) throw err;
+        out.css = value;
+      });
+
+    out.files = options._imports.map(function(item) {
+      return item.path;
+    });
+    out.files.push(filename);
+    return out;
+  }
+
   app.styleExtensions.push('.styl');
   app.compilers['.styl'] = stylusCompiler;
 };
-
-function stylusCompiler(file, filename, options) {
-  options || (options = {});
-  options._imports || (options._imports = []);
-  var out = {};
-  var s = stylus(file, options)
-    .set('filename', filename)
-    .set('compress', options.compress)
-    .set('include css', true);
-
-
-  s.render(function(err, value) {
-    if (err) throw err;
-    out.css = value;
-  });
-  out.files = options._imports.map(function(item) {
-    return item.path;
-  });
-  out.files.push(filename);
-  return out;
-}
